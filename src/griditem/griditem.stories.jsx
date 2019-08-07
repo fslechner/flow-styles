00708a8e4145a139111f779aa4a9a2f366426bfd
Frank Simon Lechner <lechner.frank@web.de>
2019-08-07 02:43:23 +0200

    refactor griditem

--- src/griditem/griditem.stories.jsx
+++ src/griditem/griditem.stories.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import cellPosition from "./cell-position.md";
-import cellResponsive from "./cell-responsive.md";
+import griditemPosition from "./griditem-position.md";
+import griditemResponsive from "./griditem-responsive.md";
 
-storiesOf("Cell", module).add(
-  "Cell Position",
+storiesOf("griditem", module).add(
+  "griditem Position",
   () => (
     <div>
       <h1>3 x 3 Grid and same value for each axis</h1>
       <div className="ce-layout">
-        <div className="ce-1 ba-petrol al-c">
-          <b>ce-1</b>
+        <div className="gri-1 ba-petrol al-c">
+          <b>gri-1</b>
         </div>
-        <div className="ce-2 ba-petrol al-c">
-          <b>ce-2</b>
+        <div className="gri-2 ba-petrol al-c">
+          <b>gri-2</b>
         </div>
-        <div className="ce-3 ba-petrol al-c">
-          <b>ce-3</b>
+        <div className="gri-3 ba-petrol al-c">
+          <b>gri-3</b>
         </div>
         <div />
         <div />
@@ -27,14 +27,14 @@ storiesOf("Cell", module).add(
       </div>
       <h1>3 x 3 Grid and different value for each axis</h1>
       <div className="ce-layout">
-        <div className="ce-v2 ce-h1 ba-petrol al-c">
-          <b>ce-v2 ce-h1</b>
+        <div className="gri-v-2 gri-h-1 ba-petrol al-c">
+          <b>gri-v-2 gri-h-1</b>
         </div>
-        <div className="ce-v3 ce-h2 ba-petrol al-c">
-          <b>ce-v3 ce-h2</b>
+        <div className="gri-v-3 gri-h-2 ba-petrol al-c">
+          <b>gri-v-3 gri-h-2</b>
         </div>
-        <div className="ce-v1 ce-h3 ba-petrol al-c">
-          <b>ce-v1 ce-h3</b>
+        <div className="gri-v-1 gri-h-3 ba-petrol al-c">
+          <b>gri-v-1 ce-h-3</b>
         </div>
         <div />
         <div />
@@ -45,11 +45,11 @@ storiesOf("Cell", module).add(
       </div>
       <h1>3 x 3 Grid and all horizontal</h1>
       <div className="ce-layout">
-        <div className="ce-h ba-petrol al-c">
-          <b>ce-h</b>
+        <div className="gri-h ba-petrol al-c">
+          <b>gri-h</b>
         </div>
-        <div className="ce-v3 ce-h ba-petrol al-c">
-          <b>ce-v3 ce-h</b>
+        <div className="gri-v-3 gri-h ba-petrol al-c">
+          <b>gri-v-3 gri-h</b>
         </div>
         <div />
         <div />
@@ -57,11 +57,11 @@ storiesOf("Cell", module).add(
       </div>
       <h1>3 x 3 Grid and all vertical</h1>
       <div className="ce-layout">
-        <div className="ce-v ba-petrol al-c">
-          <b>ce-v</b>
+        <div className="gri-v ba-petrol al-c">
+          <b>gri-v</b>
         </div>
-        <div className="ce-v ce-h3 ba-petrol al-c">
-          <b>ce-v ce-h3</b>
+        <div className="gri-v gri-h-3 ba-petrol al-c">
+          <b>gri-v ce-h-3</b>
         </div>
         <div />
         <div />
@@ -70,16 +70,16 @@ storiesOf("Cell", module).add(
       <h1>3 x 3 Grid and inividual positioning</h1>
       <div className="ce-layout">
         <div />
-        <div className="ce-vs1 ce-ve3 ce-hs2 ce-he4 ba-petrol al-c">
+        <div className="gri-vs-1 gri-ve-3 gri-hs-2 gri-he-4 ba-petrol al-c">
           <b>
-            ce-vs1 ce-ve3 <br />
-            ce-hs2 ce-he4
+            gri-vs-1 gri-ve-3 <br />
+            gri-hs-2 gri-he-4
           </b>
         </div>
-        <div className="ce-v3 ce-hs1 ce-he3 ba-petrol al-c">
+        <div className="gri-v-3 gri-hs-1 gri-he-3 ba-petrol al-c">
           <b>
-            ce-v3 <br />
-            ce-hs1 ce-he3
+            gri-v-3 <br />
+            gri-hs-1 gri-he-3
           </b>
         </div>
         <div />
@@ -88,27 +88,27 @@ storiesOf("Cell", module).add(
     </div>
   ),
   {
-    notes: { markdown: cellPosition }
+    notes: { markdown: griditemPosition }
   }
 );
 
-storiesOf("Cell", module).add(
-  "Cell Responsive",
+storiesOf("griditem", module).add(
+  "griditem Responsive",
   () => (
     <div>
       <div className="ce-layout">
-        <div className="ce-3--xsl ce-2--xsu ce-1--lgu ba-petrol al-c">
+        <div className="xsl:gri-3 xsu:gri-2 lgu:gri-1 ba-petrol al-c">
           <b>
-            ce-3--xsl <br />
-            ce-2--xsu <br />
-            ce-1--lgu
+            xsl:gri-3 <br />
+            xsu:gri-2 <br />
+            lgu:gri-1
           </b>
         </div>
-        <div className="ce-v2--xsl ce-h1--xsl ce-v1--xsu ce-h3--xsu ce-v3--lgu ce-h3--lgu ba-petrol al-c">
+        <div className="gri-v-2 gri-h-1 gri-v-1 gri-h-3 gri-v-3 gri-h-3 ba-petrol al-c">
           <b>
-            ce-v2--xsl ce-h1--xsl <br />
-            ce-v1--xsu ce-h3--xsu <br />
-            ce-v3--lgu ce-h3--lgu
+            xsl:gri-v2 xsl:gri-h1 <br />
+            xsu:gri-v1 xsu:gri-h3 <br />
+            lgu:gri-v3 lgu:gri-h3
           </b>
         </div>
         <div />
@@ -122,6 +122,6 @@ storiesOf("Cell", module).add(
     </div>
   ),
   {
-    notes: { markdown: cellResponsive }
+    notes: { markdown: griditemResponsive }
   }
 );
